faec9a88d7a3507b12f33723067b0ff8cebb124d
surajsalokhe19 <surajsalokhe90@gmail.com>
2026-02-05 15:56:14 +0530

    Add files via upload

---
+++ Python/addition.py
@@ -0,0 +1,12 @@
+    # Input from user
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Addition
+result = num1 + num2
+
+# Output
+print("Addition is:", result)
+
+
