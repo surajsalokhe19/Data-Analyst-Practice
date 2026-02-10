c89939ca87a1108f1a4418b413e1a05de253a50f
surajsalokhe19 <surajsalokhe90@gmail.com>
2026-02-10 14:08:11 +0530

    Add files via upload

---
+++ Python/list.py
@@ -0,0 +1,19 @@
+#list in python 
+
+lst = [1,2,3,"rushi","neha",34,54,32,14]
+
+#lst.append("suraj") add value at the end 
+#lst.insert(2,"Chetan") add value at particualr index
+#lst.remove("suraj") Remove the specific value from list
+#lst.pop() Remove the last value from list
+#lst.pop(2) Remove the sepcific value from the entered index 
+#lst.clear() Remove all the values from the list
+#lst.extend("Suraj","Subatta",31,19) Add multiple values at the end 
+#lst.sort() Sort the all values Inascending order from the list
+#lst.reverse() Reverse print all the values
+#lst.count(3) Count specific value occurenece from the list
+#lst.index("rushi") to find index of the specific value in the list
+
+print(lst)
+
+
