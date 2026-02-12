97c59022345437050960e25837f20816b7aff564
surajsalokhe19 <surajsalokhe90@gmail.com>
2026-02-12 15:40:25 +0530

    Add files via upload

---
+++ Python/sett.py
@@ -0,0 +1,17 @@
+# Sets in Python 
+# Closed in {} Brackets , Does not print in order 
+# Distinct values (non repeatable) 
+
+st = {1,2,3,45,"Suraj","Vexor",32.2,} 
+
+# print(st) 
+
+# st.add("Chetan") Add new value in set 
+
+# st.remove("Suraj") Remove the specific value in set
+
+# st.pop() To remove the Random value from set
+ 
+# st.clear() To remove all values from sets
+
+# st.update (["Chetan","Adity",23,45,22]) Add multiple values in set   
