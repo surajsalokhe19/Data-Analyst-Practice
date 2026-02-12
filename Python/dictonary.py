6f20b2ce74e41c1a7b485648d5652ef67720f955
surajsalokhe19 <surajsalokhe90@gmail.com>
2026-02-12 14:10:37 +0530

    Add files via upload

---
+++ Python/dictonary.py
@@ -0,0 +1,30 @@
+#Dictonary in Python 
+
+di = {
+        "name": "Suraj",
+        "age": 21,
+        "city":"Pune",
+        "add" :{"Street":"RingRoad",
+                "Pincode":416100}
+
+}
+
+# print (di.keys()) # Writes the keys only
+
+# print(di.values()) # Writes the values only
+
+# di.clear() print (di) #Clear the Dictonary
+
+# print(di.get("name")) Gives the specific value
+
+# print(di.items()) Print all the items from dict in tuple format
+
+# di.update({"name":"Surajj"}) Update the values 
+
+# di.pop("city") Removes the specific item 
+
+# print(di.popitem()) Remove the last pair item from the dictonary 
+
+# di["mob"] = 9977998899 Add new key value pair in the dictonary 
+
+# di ["add"]["landmark"] = "Near School" Add new key value pair in the dictonary
---
+++ Python/tuple.py
@@ -0,0 +1,12 @@
+#Tuple in python 
+
+# Tuples() are immutable  
+
+tp = (1,23,3,"Suraj","Vexor",45,)
+
+# print(tp)
+
+# print(tp.count(3)) To count specific value occurenece
+# print (tp.index("Suraj")) Find index of the specific value in tuple 
+
+
