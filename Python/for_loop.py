89eeb7c1724691d3991606a5eb3bfa63acdb9168
surajsalokhe19 <surajsalokhe90@gmail.com>
2026-02-25 14:12:16 +0530

    Add files via upload

---
+++ Python/for_loop.py
@@ -0,0 +1,22 @@
+# Loops are used in python to execute the block of code until certain condition is met
+# For loop is used to iterate over a sequence (list, tuple, dictonary, set or sring)
+
+
+# for i in range (1,11) : #print 1 to 10 numbers 
+#    print(i)
+
+
+# a = int(input("Enter a number:"))
+# for i in range (1,11):
+#    print(f"{a} x {i} = {a * i}")
+
+
+# for i in range (0,21,2): #(Step Function)
+#     print (i)
+
+
+# a= int (input("Enter a number:"))
+# start = int(input("Enter a Starting Range:"))
+# end = int(input("Enter the Ending Range:"))
+# for i in range (start, end+1):
+#     print(f"{a} x {i} = {a * i}")
