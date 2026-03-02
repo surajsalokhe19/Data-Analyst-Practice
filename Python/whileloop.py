f2a020f97141adcb470a4b8553aea7023ab340e1
Suraj <surajsalokhe90@gmail.com>
2026-03-02 16:01:32 +0530

    Add files via upload

---
+++ Python/whileloop.py
@@ -0,0 +1,26 @@
+# While Loop
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+
+
+# a = int(input("Enter a number : "))
+# i = 1
+# while i <=10:
+#     print (f"{a} x {i} = {a * i}")
+#     i = i + 1
+
+
+
+# start = int(input("Enter a starting range :"))
+# end = int(input("Enter Ending range:"))
+# a = int(input("Enter a number:"))
+
+# i = start
+# while i <= end:
+#     print(f"{a} x {i} = {a * i}")
+#     i = i + 1
+
