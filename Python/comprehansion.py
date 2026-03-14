eb5f4986e7e0e1bd1c86a652da78b2e55c1221b0
Suraj <surajsalokhe90@gmail.com>
2026-03-14 14:57:32 +0530

    Add files via upload

---
+++ Python/comprehansion.py
@@ -0,0 +1,12 @@
+# comprehansion in python 
+
+# logic is written first behind comprehension is to reduce the lines of code
+
+# add = [i for i in range(1,11)]  # add values from 1 to 10
+# print(add)
+
+squares = [i*i for i in range(1,11)]  #  square of values from 1 to 10
+print(squares)
+
+# a = {i : "even" if i%2==0 else "odd" for i in range(1,101)} # even and odd values from 1 to 101
+# print(a)
