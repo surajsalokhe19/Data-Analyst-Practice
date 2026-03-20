f040b764b1f1d5dcd4b0d1515e9ef2323844df2b
Suraj <surajsalokhe90@gmail.com>
2026-03-20 20:10:55 +0530

    Add files via upload

---
+++ Python/lambda.py
@@ -0,0 +1,17 @@
+# Lambda 
+# lambda is used for small functions
+
+# x = int(input("Enter a number: "))  
+# y = int(input("Enter another number: "))    
+
+# add = lambda x, y: x + y
+# sub = lambda x, y: x - y
+
+# print(add(x, y))  
+# print(sub(x, y))
+
+
+# a = int(input("Enter a number to find its square: "))
+# square = lambda x: x * x
+# print(square(a))
+
